15b52451e96cfe16ce887688b3bb0c518118002a
srv9480 <102672439+srv9480@users.noreply.github.com>
2022-10-12 13:01:25 +0300

    FEAT IC-188 Убрана обёртка у кнопки и исправлены стили

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -18,9 +18,7 @@ function Home () {
       <div className="home_main">
         <h1 className="title_text"><Trans i18nKey="header.menu.titleMain">Buy your first crypto — it’s easy</Trans></h1>
         <span className="info_text"><Trans i18nKey="header.menu.titleMainUndo">Verified by 10M+ purchases per month</Trans></span>
-        <div className="main_button">
         <a href="https://gw.indacoin.io/" className="aaa" target="_blank" style={{color: '#FFFFFF', textDecoration: 'none', fontWeight: 700, fontSize: '22px'}}><Trans i18nKey="header.menu.button">Buy crypto</Trans></a>
-        </div>
       </div>
       <h2>Our features</h2>
       <Features />
